734bf7bafd8204e5d61fe233d8a3d4401756b4ec
Simon Chae <s13chae@gmail.com>
2018-07-11 11:40:28 -0700

    added notes.test.js

--- server.js
+++ server.js
@@ -25,12 +25,12 @@ app.use(express.json());
 // Mount routers
 app.use('/api/notes', notesRouter);
 
-  // Custom 404 Not Found route handler
-  app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
+// Custom 404 Not Found route handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // Custom Error Handler
 app.use((err, req, res, next) => {
@@ -43,26 +43,22 @@ app.use((err, req, res, next) => {
   }
 });
 
-// Connect to DB and Listen for incoming connections
-mongoose.connect(MONGODB_URI)
-  .then(instance => {
-    const conn = instance.connections[0];
-    console.info(`Connected to: mongodb://${conn.host}:${conn.port}/${conn.name}`);
-  })
-  .catch(err => {
-    console.error(`ERROR: ${err.message}`);
-    console.error('\n === Did you remember to start `mongod`? === \n');
-    console.error(err);
-  });
-
-app.listen(PORT, function () {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
-});
 
 // Listen for incoming connections
 if (process.env.NODE_ENV !== 'test') {
+
+  // Connect to DB and Listen for incoming connections
+  mongoose.connect(MONGODB_URI)
+    .then(instance => {
+      const conn = instance.connections[0];
+      console.info(`Connected to: mongodb://${conn.host}:${conn.port}/${conn.name}`);
+    })
+    .catch(err => {
+      console.error(`ERROR: ${err.message}`);
+      console.error('\n === Did you remember to start `mongod`? === \n');
+      console.error(err);
+    });
+
   app.listen(PORT, function () {
     console.info(`Server listening on ${this.address().port}`);
   }).on('error', err => {
---
+++ test/notes.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const mongoose =  require('mongoose');
+
+const app = require('../server');
+
+const {TEST_MONGODB_URI} = require('../config');
+
+const Note = require('../models/note');
+
+const seedNotes = require('../db/seed/notes');
+
+const expect = chai.expect; 
+chai.use(chaiHttp);
